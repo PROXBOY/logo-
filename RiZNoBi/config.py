0f7f4a2d3283866b81a135b3f74fa4e824cb715e
RiZoeL <85222112+MrRizoel@users.noreply.github.com>
2021-07-24 23:56:53 +0530

    Update config.py

--- RiZNoBi/config.py
+++ RiZNoBi/config.py
@@ -1,26 +1,8 @@
-# RiZoeLXMusic
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-# Modified by RiZoeL
-
-import os
-from os import path
 from os import getenv
+
 from dotenv import load_dotenv
 
-if os.path.exists("local.env"):
-    load_dotenv("local.env")
-
+load_dotenv()
 que = {}
 BOT_TOKEN = getenv("BOT_TOKEN")
 BOT_NAME = getenv("BOT_NAME")
